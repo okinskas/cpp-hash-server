f9d17ba11405be2755628e6f5a235b68b01c4ad4
Ovidijus Okinskas <ovidijus1@live.com>
2019-04-22 20:38:13 +0100

    Separating projects

--- server/src/SocketReader.cpp
+++ server/src/SocketReader.cpp
@@ -14,19 +14,15 @@ std::string SocketReader::readFromSocket(int connection) {
 
     char buffer[1028];
 
+    memset(buffer, 0x00, 1028);
+
     std::size_t incoming = recv(connection, buffer, 1028, 0);
 
     if (incoming < 0) {
         throw std::runtime_error("Failed at: reading from socket.");
     }
 
-    std::string msg = getMessageFromBuffer(buffer);
+    std::string msg(buffer);
     std::cout << msg << std::endl;
     return msg;
-}
-
-std::string SocketReader::getMessageFromBuffer(char *buffer){
-
-    std::string full(buffer);
-    return full.substr(0, full.find("\\0"));
 }
