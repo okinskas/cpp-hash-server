84f78803adc66b6fd54a38725ee8c672b6d0f1a3
Ovidijus Okinskas <ovidijus1@live.com>
2019-04-26 12:25:33 +0100

    Improving deployability
    - supply env variables via .env file
    - pass variable into docker-compose.yml then Dockerfile
    - print variables in server logs to confirm
    - applies to CONCURRENT and PORT

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,13 +1,20 @@
 #include <memory>
+#include <iostream>
 
 #include "HashServer.h"
 #include "external/ShaHasher.h"
 
 int main() {
 
+    const char *concurrent = std::getenv("CONCURRENT");
+    std::cout << concurrent << std::endl;
+    const char *port = std::getenv("PORT");
+    std::cout << port << std::endl;
+    int i_int = std::atoi(concurrent);
+    int i_port = std::atoi(port);
     std::make_unique<HashServer>(
-            20,
-            8080,
+            i_int,
+            i_port,
             *std::make_unique<ShaHasher>()
     )->run();
 }
